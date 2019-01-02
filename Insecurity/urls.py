0719b9ade4b8129010cd6eb7fefbf903f44e8bf0
Akshay Brijpuria <akshaybrijpuria@Akshays-MacBook-Air.local>
2019-01-02 11:40:26 +0530

    sec

--- Insecurity/urls.py
+++ Insecurity/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import results,insecure_form_handling
+from .views import results,insecure_form_handling,search
 urlpatterns=[
         path('',results,name='results'),
-        path('form/',insecure_form_handling,name='insecure_form_handling')
+        path('form/',insecure_form_handling,name='insecure_form_handling'),
+        path('form/<str:string>',search,name='search_all')
         ]
--- Insecurity/views.py
+++ Insecurity/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.db import connection
 from .forms import InsecureForm
 def results(request):
     return render(request,'results.html',{})
@@ -15,3 +16,9 @@ def insecure_form_handling(request):
     else:
         form = InsecureForm()
         return render(request,'form.html',context={'form':form})
+
+def search(request,string):
+    with connection.cursor() as cursor:
+        cursor.execute('Select * from InsecureModel where title= %s',[string])
+        row=cursor.fetchone()
+    print(row)
